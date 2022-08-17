c7df8cd60ffee6c73297187b2a162d29cf34108d
Bryan Bosire <bryanbosire@gmail.com>
2022-08-17 23:01:37 +0300

    commit tree 5339c88252b1010a8eeed752711e8d1054f0ff91

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+def line(array)
+  if array.empty?
+    puts "The line is currently empty."
+  else
+    list = array.map.with_index(1) {|name, index| "#{index}. #{name}"}
+    puts "The line is currently: " + list.join(" ")
+  end
+end
+
+def take_a_number(array, name)
+    array << name
+    puts "Welcome, #{name}. You are number #{array.length} in line."
+end
+
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+
+end
+
